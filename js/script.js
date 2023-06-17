c111109e511468435903e197de6ba9f02a2420b9
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-17 19:05:35 +0200

    Ciclo for

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 // Creare array con lista delle 5 immagini
 
-const img = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp', ];
+const allImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp', ];
+console.log(allImg, typeof allImg);
+
+
+// Ciclo for per concatenare Template Literal
+
+
+for (let i = 0; i < allImg.length; i++) {
+    console.log (allImg[i]);
+  
+
+}
+
+
+
+
 
