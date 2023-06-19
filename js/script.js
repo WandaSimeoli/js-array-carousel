de1107df8ea44250f4d2c1a9f35e304ad840952f
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-19 19:27:18 +0200

    Completata la mailstone 2

--- js/script.js
+++ js/script.js
@@ -8,20 +8,40 @@ console.log(allImg, typeof allImg);
 
 const content = document.querySelector('.content');
 
+let activeElement = 0;
+
 for (let i = 0; i < allImg.length; i++) {
     console.log (allImg[i]);
     
     if (i == 0) {
     content.innerHTML = content.innerHTML + `
-    <img src="${allImg[i]}" class="active" alt="">`; 
+    <div>
+    <img src="${allImg[i]}" class="active" alt="">
+    </div>`; 
 }
 else {
     content.innerHTML = content.innerHTML  + `
-    <img src="${allImg[i]}" alt="">`; 
+    <div>
+    <img src="${allImg[i]}" alt="">
+    </div>`; 
 }
 }
 
+const allContent = document.querySelectorAll('img');
+console.log(allContent, typeof allContent);
+ 
 
+const postArrow = document.querySelector('.post');
+
+postArrow.addEventListener('click', 
+   function () {
+    console.log('click post', activeElement);
+    allContent[activeElement].classList.remove('active');
+
+    console.log('dopo il click', activeElement + 1);
+    allContent [activeElement].classList.add('active');
+    }
+);
 
 
 
